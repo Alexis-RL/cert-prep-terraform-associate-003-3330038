3799db03db477a3463adc5a2074ed8faf9918061
Alexis-RL <alexisruizlopez2108@gmail.com>
2024-12-04 13:33:59 -0600

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "my-instance-alexis" {
     #instance configuration
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-instance-alexis"
+  }
 }
